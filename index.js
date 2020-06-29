42e2bc1a0df6450a866df04e284b3cb9a784c875
junior <pomarancze30@gmail.com>
2020-06-29 13:21:36 +0200

    nowe pliki

--- index.js
+++ index.js
@@ -122,9 +122,6 @@ class Game extends React.Component {
     );
   }
 }
-
-// ========================================
-
 ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(squares) {
